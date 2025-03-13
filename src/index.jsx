f8a29fcdd45c26d981310b5cb89e89971b1e1ace
l-nikola <legay.nikola.pro@gmail.com>
2025-03-13 16:55:36 +0100

    feat : arborescence + routing

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/";
+import About from "./pages/About";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
