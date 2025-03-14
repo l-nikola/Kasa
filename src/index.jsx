55d7f4163f11de98cd1086cb2e2b18f04376453f
l-nikola <legay.nikola.pro@gmail.com>
2025-03-14 09:57:52 +0100

    feat : add error route

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   </React.StrictMode>
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Error</h1>
+    </>
+  );
+}
+
+export default Home;
