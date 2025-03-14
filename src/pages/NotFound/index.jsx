6804026db8f167ed7dbaaef316111f6b5870e9d0
l-nikola <legay.nikola.pro@gmail.com>
2025-03-14 15:19:30 +0100

    feat : error page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Error() {
   return (
-    <>
-      <h1>Error</h1>
-    </>
+    <main className="error">
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <Link className="error__button" to="/">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
   );
 }
 
