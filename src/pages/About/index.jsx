25e2bf6cb234269e26c560938be3ce20280b2870
l-nikola <legay.nikola.pro@gmail.com>
2025-03-17 22:27:51 +0100

    fix : remove useless + sort code

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,10 @@
+import Banner from "../../components/Banner";
+
 function About() {
   return (
     <div>
       <h1>About</h1>
+      <Banner image="public\Image source 2.svg" />
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,13 +4,10 @@ import Card from "../../components/Card";
 function Home() {
   return (
     <>
-      {/* Ma première image avec texte */}
       <Banner
         image="public\Image source 1.svg"
         text="Chez vous, partout et ailleurs"
       />
-      {/* Ma deuxième image sans texte */}
-      {/* <Banner image="public\Image source 2.svg" /> */}
 
       <div className="cardContainer">
         <Card />
