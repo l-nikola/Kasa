549c0ebf7903bbe81625545c005bd2d1b4583a18
l-nikola <legay.nikola.pro@gmail.com>
2025-03-17 22:23:14 +0100

    feat : import card component in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,25 @@
 import Banner from "../../components/Banner";
+import Card from "../../components/Card";
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
       {/* Ma première image avec texte */}
       <Banner
         image="public\Image source 1.svg"
         text="Chez vous, partout et ailleurs"
       />
       {/* Ma deuxième image sans texte */}
-      <Banner image="public\Image source 2.svg" />
+      {/* <Banner image="public\Image source 2.svg" /> */}
+
+      <div className="cardContainer">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 }
