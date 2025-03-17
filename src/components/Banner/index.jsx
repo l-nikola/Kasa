1f84f6df079f3f1fb7428ffb3f84d7f501e83cb2
l-nikola <legay.nikola.pro@gmail.com>
2025-03-17 21:29:22 +0100

    feat : banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,7 @@
 function Banner({ image, text }) {
+  const brightnessClass = text ? "banner__text" : "";
   return (
-    <section className="banner">
+    <section className={`banner ${brightnessClass}`}>
       <img src={image} alt={text} />
       {text && <p>{text}</p>}
     </section>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,9 +5,12 @@ function Home() {
     <>
       <h1>Home</h1>
       {/* Ma première image avec texte */}
-      <Banner image="public\Image source 1.svg" text="Mon texte" />
+      <Banner
+        image="public\Image source 1.svg"
+        text="Chez vous, partout et ailleurs"
+      />
       {/* Ma deuxième image sans texte */}
-      <Banner image="public\Image source 2.svg" text="" />
+      <Banner image="public\Image source 2.svg" />
     </>
   );
 }
