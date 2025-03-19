7679df3f8ac0ad1c78e8620ce91611882bcefcb6
l-nikola <legay.nikola.pro@gmail.com>
2025-03-19 22:06:53 +0100

    feat : responsive error page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -4,7 +4,10 @@ function Error() {
   return (
     <main className="error">
       <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <h2>
+        Oups! La page que <br className="mobile-break" />
+        vous demandez n'existe pas.
+      </h2>
       <Link className="error__button" to="/">
         Retourner sur la page d’accueil
       </Link>
