99ea9a6c2a7e7b0f2733bae8a604d85878ff74fd
l-nikola <legay.nikola.pro@gmail.com>
2025-03-20 18:28:25 +0100

    [WIP] feat : collapse component + import on about page

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,10 @@
+function Collapse({ text }) {
+  return (
+    <div className="collapse">
+      <span>{text}</span>
+      <i class="fa-solid fa-chevron-up"></i>
+    </div>
+  );
+}
+
+export default Collapse;
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,20 @@
 import Banner from "../../components/Banner";
+import Collapse from "../../components/Collapse";
+import CollapseData from "../../../public/collapses.json";
 
 function About() {
   return (
     <div>
-      <h1>About</h1>
       <Banner image="public\Image source 2.svg" />
+      <section className="collapseContainer">
+        {CollapseData.map((data) => {
+          return (
+            <div key={data.id}>
+              <Collapse text={data.title} />
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
