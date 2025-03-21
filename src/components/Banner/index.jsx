64fd71e1537c5dbb32b80cdc055fddb4fcb2e64c
l-nikola <legay.nikola.pro@gmail.com>
2025-03-21 07:33:14 +0100

    fix : replace section by figure Banner componnent

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,10 @@
 function Banner({ image, text }) {
   const brightnessClass = text ? "banner__text" : "";
   return (
-    <section className={`banner ${brightnessClass}`}>
+    <figure className={`banner ${brightnessClass}`}>
       <img src={image} alt={text} />
-      <p>{text}</p>
-    </section>
+      <figcaption>{text}</figcaption>
+    </figure>
   );
 }
 
