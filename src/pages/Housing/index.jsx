dcdb8316b6904cc520b77997d83801a3f0ec9b90
l-nikola <legay.nikola.pro@gmail.com>
2025-03-24 19:52:17 +0100

    [WIP] feat : Housing page

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Slideshow from "../../components/Slideshow";
 import logements from "../../../public/data/logement.json";
+import Slideshow from "../../components/Slideshow";
+import Collapse from "../../components/Collapse";
 
 function Housing() {
   const { id } = useParams();
   const logement = logements.find((item) => item.id === id);
 
   return (
-    <main>
+    <main className="housing">
       <Slideshow pictures={logement.pictures} />
+      <h1>{logement.title}</h1>
+      {/* // Avatar components // */}
+      <span>{logement.location}</span>
+      {/* // Tags components //  */}
+      {/* // Rating components // */}
+      <section className="housing__collapse">
+        <Collapse title="Description" content={logement.description} />
+        <Collapse
+          title="Équipements"
+          content={
+            <ul>
+              {logement.equipments.map((equipment, index) => (
+                <li key={index}>{equipment}</li>
+              ))}
+            </ul>
+          }
+        />
+      </section>
+
+      <br />
     </main>
   );
 }
